58d3186cd1aeb28336bd3c0a129b82dbd9be8f9d
Andy Loftus <aloftus@gmail.com>
2021-02-22 12:05:08 -0600

    Add support for branch "main"

--- README.md
+++ README.md
@@ -7,17 +7,17 @@ Boilerplate for a repo with quickstart supporting `curl | bash` syntax
     - This bash script should do all the work of installing your repo
     - Ensure setup.sh is executable
   - Copy the lines below into your README
-  - Replace QS_REPO with your repo url
+  - Replace QS_GIT_REPO with your repo url
 ```bash
-export QS_REPO=https://github.com/andylytical/quickstart
-#export QS_BRANCH=branch_name  #optional - specify a branch other than master
+export QS_GIT_REPO=https://github.com/andylytical/quickstart
+#export QS_GIT_BRANCH=branch_name  #optional - specify a branch other than master
 curl https://raw.githubusercontent.com/andylytical/quickstart/master/quickstart.sh | bash
 ```
 
 # Example Usage
 ```bash
-export QS_REPO=https://github.com/andylytical/quickstart
-#export QS_BRANCH=branch_name  #optional - specify a branch other than master
+export QS_GIT_REPO=https://github.com/andylytical/quickstart
+#export QS_GIT_BRANCH=branch_name  #optional - specify a branch other than master
 curl https://raw.githubusercontent.com/andylytical/quickstart/master/quickstart.sh | bash
 ```
 - Copy/paste the above lines in your Bash terminal window
--- quickstart.sh
+++ quickstart.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Allow QS_REPO and QS_GIT_REPO
+if [[ -n "${QS_GIT_REPO}" ]] ; then
+  REPO="${QS_GIT_REPO}"
+else
+  REPO="${QS_REPO}"
+fi
+
 # Branch to clone from
-BRANCH=${QS_GIT_BRANCH:-master}
-[[ "$QS_BRANCH" ]] && BRANCH="$QS_BRANCH"
+if [[ -n "${QS_GIT_BRANCH}" ]] ; then
+  BRANCH="${QS__GIT_BRANCH}"
+elif [[ -n "${QS_BRANCH}" ]] ; then
+  BRANCH="${QS_BRANCH}"
+else
+  BRANCH=main
+fi
 
 # Temp working space
 TMPDIR=$(mktemp -d)
@@ -11,46 +23,77 @@ DEBUG=1
 
 # FUNCTIONS
 cleanup() {
-    [[ $DEBUG -gt 0 ]] && set -x
-    rm -rf "$TMPDIR"
+  [[ $DEBUG -gt 0 ]] && set -x
+  rm -rf "$TMPDIR"
 }
 
 
 die() {
-    echo "Error: ${*}" >&2
-    cleanup
-    exit 2
+  echo "Error: ${*}" >&2
+  cleanup
+  exit 2
 }
 
 
 check_git() {
-    [[ $DEBUG -gt 0 ]] && set -x
-    which git || die "Can't find 'git'. Is 'git' installed and on your path?"
+  [[ $DEBUG -gt 0 ]] && set -x
+  which git || die "Can't find 'git'. Is 'git' installed and on your path?"
 }
 
 
 validate_repo() {
-    [[ $DEBUG -gt 0 ]] && set -x
-    [[ -z "$QS_REPO" ]] && die "QS_REPO not defined."
-    git ls-remote "$QS_REPO" &>/dev/null \
-        || die "Invalid or Inaccessible repository: '$QS_REPO'"
+  [[ $DEBUG -gt 0 ]] && set -x
+  [[ -z "$REPO" ]] && die "QS_GIT_REPO not defined."
+  git ls-remote "$REPO" &>/dev/null \
+  || die "Invalid or Inaccessible repository: '$REPO'"
+}
+
+
+validate_branch() {
+  [[ $DEBUG -gt 0 ]] && set -x
+  [[ -z "$BRANCH" ]] && die "QS_GIT_BRANCH not defined."
+  local _is_valid=no
+  if is_remote_branch_valid "$BRANCH" ; then
+    _is_valid=yes
+  else
+    if [[ "$BRANCH" == "main" ]] ; then
+      # try for master branch (instead of main)
+      if is_remote_branch_valid "master" ; then
+        BRANCH="master"
+        _is_valid=yes
+      fi
+    fi
+  fi
+  if [[ "$_is_valid" == "no" ]] ; then
+    die "Invalid branch: '$BRANCH'"
+  fi
+}
+
+
+is_remote_branch_valid() {
+  [[ $DEBUG -gt 0 ]] && set -x
+  local _branch="$1"
+  [[ -z "$_branch" ]] && die "Refusing to test empty branch name"
+  git ls-remote --heads "$REPO" "$_branch" 2>/dev/null \
+  | head -1 \
+  | grep -q "refs/heads/$_branch"
 }
 
 
 clone_repo() {
-    [[ $DEBUG -gt 0 ]] && set -x
-    git clone \
-        --single-branch \
-        --branch "$BRANCH" \
-        "$QS_REPO" \
-        $TMPDIR
+  [[ $DEBUG -gt 0 ]] && set -x
+  git clone \
+    --single-branch \
+    --branch "$BRANCH" \
+    "$REPO" \
+    $TMPDIR
 }
 
 run_setup() {
-    [[ $DEBUG -gt 0 ]] && set -x
-    local _setup=$TMPDIR/setup.sh
-    [[ -f "$_setup" ]] || die "Setup script not found: '$_setup'"
-    "$_setup"
+  [[ $DEBUG -gt 0 ]] && set -x
+  local _setup=$TMPDIR/setup.sh
+  [[ -f "$_setup" ]] || die "Setup script not found: '$_setup'"
+  "$_setup"
 }
 
 [[ $DEBUG -gt 0 ]] && set -x
@@ -58,6 +101,7 @@ run_setup() {
 check_git
 
 validate_repo
+validate_branch
 
 clone_repo
 
